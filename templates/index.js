c5cc34d51b30bb89e853d5c63a7624d4e826a95d
Amala Shwetah <93658002+Amala02@users.noreply.github.com>
2023-04-25 20:12:04 +0530

    commit

---
+++ templates/index.js
@@ -0,0 +1,14 @@
+function signup(){
+	document.getElementById('signup').style.display = "block"
+	
+}
+function login(){
+	document.getElementById('login').style.display="block"
+}
+
+function open_emoter(){
+	document.getElementById('emoter-working').style.display=""
+
+}
+
+
